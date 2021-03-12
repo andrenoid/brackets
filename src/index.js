1a5ec5d818d7ebfb861e84ea97d899001348c687
andrenoid <79751083+andrenoid@users.noreply.github.com>
2021-03-12 21:34:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,15 +6,5 @@ module.exports = function check(str, bracketsConfig) {
   if (str.length % 2 !== 0) {
     return false
   }
-    for (let i = 0; i < str.length; i++) {
-        for (let j = 0; j < bracketsConfig.length; j++) {
-            if (arr[arr.length - 1] == bracketsConfig[j][0] && str[i] == bracketsConfig[j][1]) {
-              arr.pop();
-            } else if (str[i] == bracketsConfig[j][0]) {
-              arr.push(str[i]);
-            }
-        }
-    } if (arr.length == 0) {
-      return true;
-    } return false;
+
 }
